013f8b2ef26c4062ac3cf3c541e4c881067339aa
sergei <33bodley33@gmail.com>
2025-01-16 14:10:56 +0700

    c1

---
+++ dist/script.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/
+let a = 2 + 2;
+console.log(a);
+/******/ })()
+;
+//# sourceMappingURL=script.js.map
